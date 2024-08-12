47eca1324ad84cc83e057e2fa9638aad073bc9a1
= <=>
2024-08-12 20:13:39 -0300

    test: :white_check_mark: added  tests
    added tests for lambda index

---
+++ lambda/index.test.ts
@@ -0,0 +1,8 @@
+import { sun } from "./index"
+
+
+it('should sum', ()=>{
+    const res = sun(1,2)
+    expect(res).toEqual(3)
+})
+;
---
+++ lambda/index.ts
@@ -0,0 +1,6 @@
+const sun = (n1:number, n2:number):number=>{
+    return n1 + n2
+}
+export {
+    sun
+}
